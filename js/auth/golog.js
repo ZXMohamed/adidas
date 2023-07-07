0570aa45c255c3c8347373a643490d54e8575737
ZXMohamed <99717151+ZXMohamed@users.noreply.github.com>
2023-07-07 09:39:32 -0700

    Update golog.js

--- js/auth/golog.js
+++ js/auth/golog.js
@@ -2,5 +2,5 @@ import {setgolog} from "../cookie/cookiecontrole.js";
 const navloginbutton = document.getElementById("navloginbutton");
 navloginbutton.addEventListener("click", function () { 
   setgolog("t");
-  window.location.assign("../../index.html");
+  window.location.assign("../index.html");
 })
